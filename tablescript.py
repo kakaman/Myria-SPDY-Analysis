959f840f74d39e832ac8267286a4b3b8c9ee2128
Vyshnav Kakivaya <vyshnav@msn.com>
2014-07-31 10:59:08 -0700

    Added to the tablescript.py file.

--- tablescript.py
+++ tablescript.py
@@ -5,21 +5,42 @@ data = []
 for row in json_data:
 	data.append(row)
 
-## Different method
 
 import json as j
 datafile = open('C:\Users\Vyshnav\Documents\GitHub\Myria-SPDY-Analysis\data\wprof_300_5_pro', 'r')
-json_data = j.JSONDecoder(datafile)
+json_data = j.loads(datafile)
 
-for row in json_data:
-		if row[0] == 'Resource':
+ObjectHashesDict = {}
+RecievedChunksDict = {}
+HOLsDict = {}
+ComputationsDict = {}
+PreloadsDict = {}
+ResourcesDict = {}
 
-		elif row[0] == 'ObjectHash':
-		
-		elif row[0] == 'RecievedChunk':
-		
-		elif row[0] == 'HOL':
-		
-		elif row[0] == 'Computation':
-		
-		elif row[0] == 'Preloads': # or use else:
+copydict = lambda dct, keys: {key: dct[key] for key in keys}
+
+ObjectHashesDict = copydict(json_data, 'ObjectHash')
+RecievedChunksDict = copytdict(json_data, 'RecievedChunk')
+HOLsDict = copytdict(json_data, 'HOL')
+ComputationsDict = copytdict(json_data, 'Computation')
+PreloadsDict = copyDict(json_data, 'Preloads')
+ResourcesDict = copytdict(json_data, 'Resource')
+
+HOLsDict['Hol'].append[] # Append a value for each HOL making it easier to ID
+
+RecievedChunksDict['RecievedChunk'].append # Add ResourceID param to the  Recieved Chunks
+
+
+
+# Adding ResourceIDs to RecievedChunks
+# do it when originally parsing
+# i.e find the resource before it and add its id
+
+rID = 0
+
+for keys, values in json_data:
+	if key == 'Resource':
+		rID = json_data['Resource']['id']
+
+	if key == 'RecievedChunk':
+		json_data['RecievedChunk']['ResourceID'] = rID 
