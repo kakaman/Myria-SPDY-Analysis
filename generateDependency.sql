e1ff5dbdd353d168c5be4c7f1eaa67bdbe9a3031
Vyshnav Kakivaya <vyshnav@msn.com>
2014-08-05 09:57:00 -0700

    Modified generateDependency.sql

--- generateDependency.sql
+++ generateDependency.sql
@@ -12,10 +12,19 @@ receivedchunks = empty(ResourceID: int, receivedTime: real, PageUrl: string, len
 preloads = empty(url: string, docUrl: string, code: int, PageUrl: string, time: real);
 
 -- Take out the ask.fm information out of each table
-AskComp = select PageUrl from computations c where c.PageUrl = "ask.fm";
-AskHol = select PageUrl from hol h where h.PageUrl = "ask.fm";
-AskRes = select PageUrl from resources r where r.PageUrl = "ask.fm";
-AskRC = select PageUrl from receivedchunks rc where rc.PageUrl = "ask.fm";
-AskPre = select PageUrl from preloads p where p.PageUrl = "ask.fm";
+Comp = select * from computations c where c.PageUrl = "ask.fm";
+Hol = select * from hol h where h.PageUrl = "ask.fm";
+Res = select * from resources r where r.PageUrl = "ask.fm";
+RC = select * from receivedchunks rc where rc.PageUrl = "ask.fm";
+Pre = select * from preloads p where p.PageUrl = "ask.fm";
 
--- Creates the downloads
+
+-- Generates the large "combo" table
+Combo = select * from Comp, Pre, RC, Res, Hol;
+
+-- Creates the "download" from the Resources
+download = select s_time, id, type from Res;
+
+-- Creates the "comps" table from the computations, Resources
+
+comps = select s_time, time, "r1_c1" as id, type, e_time from Res, Comp where Res.sentTime = Comp.startTime; -- The ID has to change/increment for each one.
